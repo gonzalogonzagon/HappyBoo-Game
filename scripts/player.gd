7d9cf0139307b7e0ea755b1b57d2cdbc50ca34d1
Gonza Rico <gonzaricoesteban@gmail.com>
2024-08-28 20:23:27 +0200

    Mob added

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ func _physics_process(delta):
 	var direction = Input.get_vector(
 						"move_left","move_right","move_up","move_down"
 									)
-	velocity = direction * 600
+	velocity = direction * 600.0
 	move_and_slide()
 	
 	if velocity.length() > 0.0:
