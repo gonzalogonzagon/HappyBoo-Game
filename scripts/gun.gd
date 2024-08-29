7dd5c78c70127f9723eacff6a340f867e3ea72a9
Gonza Rico <gonzaricoesteban@gmail.com>
2024-08-29 18:06:59 +0200

    Shooting mecanic added

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -5,3 +5,10 @@ func _physics_process(delta):
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+		
+func shoot():
+#you can also use preload(), this is a static function that will be executed as soon as the game and this file starts
+	var new_bullet = load("res://scenes/bullet.tscn").instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
+const SPEED = 600.0
+
 func _physics_process(delta):
 	var direction = Input.get_vector(
-						"move_left","move_right","move_up","move_down"
+		"move_left","move_right","move_up","move_down"
 									)
-	velocity = direction * 600.0
+	velocity = direction * SPEED
 	move_and_slide()
 	
 	if velocity.length() > 0.0:
 		$HappyBoo.play_walk_animation()
 	else:
 		$HappyBoo.play_idle_animation()
+		
+	if Input.is_action_just_pressed("shoot"):
+		$Gun.shoot()
